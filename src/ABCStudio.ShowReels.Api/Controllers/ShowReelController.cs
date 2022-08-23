e33ade2d68b362ee179efd6c9610636b0daf1c94
Piyumi Gunarathna <piyumi.gunarathna@gmail.com>
2022-08-23 23:11:42 +0800

    Add controller get/post actions

---
+++ src/ABCStudio.ShowReels.Api/Controllers/ShowReelController.cs
@@ -0,0 +1,55 @@
+using ABCStudio.ShowReels.Application.DTOs;
+using ABCStudio.ShowReels.Application.Services;
+using ABCStudio.ShowReels.Core.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABCStudio.ShowReels.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShowReelController : ControllerBase
+    {
+        private readonly ShowReelService _showReelService;
+
+        public ShowReelController(ShowReelService showReelService)
+        {
+            _showReelService = showReelService;
+        }
+
+        [HttpGet]
+        public ObjectResult Get()
+        {
+            try
+            {
+                var response = _showReelService.GetShowReels();
+                return new ObjectResult(response);
+            }
+            catch (Exception ex)
+            {
+                var errorResponse = new ObjectResult("Error occured.");
+                errorResponse.StatusCode = 500;
+                return errorResponse;
+            }
+        }
+
+        [HttpPost]
+        public ObjectResult Post(ShowReelDTO request)
+        {
+            try
+            {
+                var response = _showReelService.CreateShowReel(request);
+                return new ObjectResult(response);
+            }
+            catch (ShowReelException ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                var errorResponse = new ObjectResult("Error occured.");
+                errorResponse.StatusCode = 500;
+                return errorResponse;
+            }
+        }
+    }
+}
