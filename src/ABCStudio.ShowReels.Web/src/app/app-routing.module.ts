e1d1935fdcdd9466f710fb671d57c5102f209592
Piyumi Gunarathna <piyumi.gunarathna@gmail.com>
2022-08-24 22:56:45 +0800

    Add routing

--- src/ABCStudio.ShowReels.Web/src/app/app-routing.module.ts
+++ src/ABCStudio.ShowReels.Web/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShowReelListComponent } from './components/show-reel-list/show-reel-list.component';
+import { ShowReelComponent } from './components/show-reel/show-reel.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'new-show-reel', component: ShowReelComponent},
+  { path: 'edit-show-reel', component: ShowReelComponent},
+  { path: 'show-reel-list', component: ShowReelListComponent},
+  { path: '**', component: ShowReelListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
